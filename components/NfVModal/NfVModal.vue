28bff0daef25fac74d12b9551a769a3764b77aa2
nfeng <308561157@qq.com>
2020-01-17 11:57:53 +0800

    1.0.34

--- components/NfVModal/NfVModal.vue
+++ components/NfVModal/NfVModal.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="v-modal"
+    class="nf-v-modal"
     v-show="showVModal"
     @click="showVModal = false"
     :style="{ backgroundImage: imgList.length ? `url(${imgList[currentIndex - 1]})` : ''}">
@@ -36,42 +36,47 @@ export default {
 }
 </script>
 
-<style scoped>
-.v-modal {
+<style lang="scss" scoped>
+.nf-v-modal {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  opacity: .5;
+  background: #000;
   z-index: 2001;
-  opacity: 1;
-  background-color: rgba(0, 0, 0, 0.5);
   background-image: url(../../image/example.png);
   background-repeat: no-repeat;
-  background-size: 800px auto;
   background-position: center center;
   transition: all 0.3s;
-}
-.left {
-  position: absolute;
-  top: calc(50% - 40px);
-  left: 40px;
-  width: 80px;
-  height: 80px;
-  background-image: url(../../image/left.png);
-  background-size: cover;
-  cursor: pointer;
-}
-.right {
-  position: absolute;
-  top: calc(50% - 40px);
-  right: 40px;
-  width: 80px;
-  height: 80px;
-  background-image: url(../../image/right.png);
-  background-size: cover;
-  cursor: pointer;
-}
-.total {
-  position: absolute;
-  width: 100%;
-  bottom: 40px;
-  text-align: center;
-  color: #fff;
+
+  .left {
+    position: absolute;
+    top: calc(50% - 40px);
+    left: 40px;
+    width: 80px;
+    height: 80px;
+    background-image: url(../../image/left.png);
+    background-size: cover;
+    cursor: pointer;
+  }
+  .right {
+    position: absolute;
+    top: calc(50% - 40px);
+    right: 40px;
+    width: 80px;
+    height: 80px;
+    background-image: url(../../image/right.png);
+    background-size: cover;
+    cursor: pointer;
+  }
+  .total {
+    position: absolute;
+    width: 100%;
+    bottom: 40px;
+    text-align: center;
+    color: #fff;
+  }
 }
 </style>
