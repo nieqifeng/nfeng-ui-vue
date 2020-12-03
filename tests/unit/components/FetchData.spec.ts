8c3cbdbf9380f419ff33a1bda1649cf32be9f0cf
N-feng <308561157@qq.com>
2020-12-03 10:49:23 +0800

    完善jest测试实例

--- tests/unit/components/FetchData.spec.ts
+++ tests/unit/components/FetchData.spec.ts
@@ -1,5 +1,6 @@
 import { mount } from '@vue/test-utils'
-import Vue from 'vue'
+import flushPromises from 'flush-promises'
+// import Vue from 'vue'
 import FetchData from '@/components/FetchData.vue'
 jest.mock('@/utils', () => ({
   getRandomNum: () => 6
@@ -9,18 +10,28 @@ jest.mock('@/api', () => ({
 }))
 
 describe('FetchData.vue', () => {
-  // it('在created生命周期中调用方法', () => {
-  //   const getUser = jest.fn()
-  //   const options = {
-  //     methods: { getUser }
-  //   }
-  //   mount(FetchData, options)
-  //   expect(getUser).toBeCalled()
-  // })
-  it('保存异步接口的返回值', () => {
-    const wrapper = mount(FetchData)
-    return Vue.nextTick().then(() => {
-      expect(wrapper.vm.$data.msg).toMatch('~!@#$%^&*()_+')
+  it('在created生命周期中调用方法', async () => {
+    const getUser = jest.fn()
+    // console.log('jest.fn(): ', jest.fn());
+    // console.log('getUser: ', getUser);
+    // const options = {
+    //   methods: { getUser }
+    // }
+    // mount(FetchData, options)
+    // expect(getUser).toBeCalled()
+    const wrapper = mount(FetchData, {
+      mocks: {
+        $post: getUser // 应用该mock函数
+      }
     })
+    await flushPromises()
+    // console.log(wrapper.vm.$data.msg)
+    expect(wrapper.vm.$data.msg).toMatch('~!@#$%^&*()_+')
   })
+  // it('保存异步接口的返回值', () => {
+  //   const wrapper = mount(FetchData)
+  //   return Vue.nextTick().then(() => {
+  //     expect(wrapper.vm.$data.msg).toMatch('~!@#$%^&*()_+')
+  //   })
+  // })
 })
